0231cdbd28659453e14e42b4722073c65d10d6eb
Level420 <level420@protonmail.com>
2021-12-22 18:32:27 +0000

    added stuff

--- api/v1/login.php
+++ api/v1/login.php
@@ -1,3 +1,4 @@
+<?php
 $email = $_POST['email'];
 $password = $_POST['email'];
 
@@ -9,14 +10,22 @@ $preparestring1 = pg_prepare($conn, 'query1', 'SELECT email FROM users where ema
 $result1 = pg_execute($conn, 'query1', array($email));
 $numrows = pg_numrows($result1);
 if ($numrows != 1) {
-returnobj = new \stdClass();
-returnobj->success = "false";
-returnobj->reason = "no_account";
-returnobj = json_encode(returnobj);
+$returnobj = new \stdClass();
+$returnobj->success = "false";
+$returnobj->reason = "no_account";
+$returnobj = json_encode(returnobj);
 echo returnobj;
 die();
 }
 else {
-preparestring2 = pg_prepare($conn, 'query2' 'SELECT password, salt, token FROM users where email = $1')
-result2 = pg_execute($conn, 'query2', array($email));
+$preparestring2 = pg_prepare($conn, 'query2' 'SELECT password, salt, token FROM users where email = $1')
+$result2 = pg_execute($conn, 'query2', array($email));
+$passwordhash = pg_fetch_result($conn, 0, 0);
+$salt = pg_fetch_result($conn, 0, 1);
+$token = pg_fetch_result($conn, 0, 2);
+echo $passwordhash;
+echo $salt;
+echo $token;
 
+}
+?>
