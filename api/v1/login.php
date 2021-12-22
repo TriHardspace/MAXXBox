e273d304a97e9ec0a3f16550b6562eba3a3ee96d
Level420 <level420@protonmail.com>
2021-12-22 03:27:39 +0000

    Added basic login utility (still testing)

---
+++ api/v1/login.php
@@ -0,0 +1,22 @@
+
+
+
+function getSalt($n) {
+    $characters = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+    $randomString = '';
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+
+    }
+
+    return $randomString;
+};
+
+
+$username = $_POST['email'];
+$password = $_POST['password'];
+$salt = getSalt(strlen($password);)
+$password .= $salt;
+print($password);
+
