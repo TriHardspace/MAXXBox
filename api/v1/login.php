012a6a9069231650040ee001ee74831ebb0297e0
Level420 <level420@protonmail.com>
2021-12-22 14:37:42 +0000

    reverting back to what worked somewhat :(

--- api/v1/login.php
+++ api/v1/login.php
@@ -28,8 +28,7 @@ $pguser = getenv("POSTGRES_USER");
 $pgpassword = getenv("POSTGRES_PASSWORD");
 $dbname = getenv("POSTGRES_DB");
 $connectstring = getenv('CONNECT_STRING');
-print($connectstring);
-$conn = pg_connect(getenv('CONNECT_STRING'));
+$conn = pg_connect($connectstring);
 $result = pg_prepare($conn, "query1", "SELECT email FROM USERS WHERE email = $email");
 if (in_array($email, $result) == True) {
 print("You are already registered");
@@ -38,6 +37,7 @@ die();
 else {
 print("unfinished");
 // THIS PART NEEDS TO BE WORKED ON STILL //
+
 }
 
 }
