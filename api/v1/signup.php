bb3f15802cd4d7001fdab67401068832ddac58ab
Level420 <level420@protonmail.com>
2021-12-25 03:28:39 +0000

    got some weird ass errors with that so I'm not gonna fw adding a creation date variable

--- api/v1/signup.php
+++ api/v1/signup.php
@@ -45,10 +45,8 @@ echo $returnobj;
 die();
 }
 else {
-$insertstring1 = pg_prepare($conn, "insert1", "INSERT INTO users (email, password, token, salt, creation_date) VALUES ($1, $2, $3, $4, $5)");
-$date = date('yyyy/mm/dd');
-print($date);
-$result2 = pg_execute($conn, "insert1", array($email, $password, $token, $salt, $date));
+$insertstring1 = pg_prepare($conn, "insert1", "INSERT INTO users (email, password, token, salt, creation_date) VALUES ($1, $2, $3, $4)");
+$result2 = pg_execute($conn, "insert1", array($email, $password, $token, $salt));
 $returnobj = new \stdClass();
 // Just some bullshit to make php shut up
 $returnobj->token = $token;
