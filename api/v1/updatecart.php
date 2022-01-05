6c46061a63bf6f5a3a95d4e869aff48652f611d7
Level420 <level420@protonmail.com>
2022-01-05 21:27:41 +0000

    trolling.mp4

---
+++ api/v1/updatecart.php
@@ -0,0 +1,32 @@
+<?php
+$hetzner_starter = $_POST['hetzner_starter'];
+$hetzner_plus = $_POST['hetzner_plus'];
+$hetzner_advanced = $_POST['hetzner_advanced']
+$token = $_POST['token'];
+$prices = array(9.69, 17.69, 27.69);
+// first is starter, second is plus, third is advanced 
+$connectstring = fread($connectfile, filesize("/var/www/nonpublic/connect.txt"));
+$conn = pg_connect($connectstring);
+$preparestring1 = pg_prepare($conn, 'query1', 'SELECT email FROM users WHERE token=$1');
+$result1 = pg_execute($conn, 'query1', array($token));
+$numrows = pg_numrows($result1);
+if (numrows != 1) {
+$returnobj = new \stdClass();
+$returnobj->success = "false";
+$returnobj->reason = "invalid_token";
+else {
+$hetzner_starter_total = $hetzner_starter * prices[0];
+$hetzner_plus_total = $hetzner_plus * prices[1];
+$hetzner_advanced_total = $hetzher_advanced * prices[2];
+$email = pg_fetch_result($result1, 0, 0);
+$preparestring2 = pg_prepare($conn, 'update1', 'UPDATE cart SET (hetzner_starter, hetzner_plus, hetzner_advanced, total, subtotal) = ($1, $2, $3) WHERE email = $4');
+$result2 = pg_execute($conn, 'update1', array());
+}
+
+}
+
+
+
+
+
+?>
