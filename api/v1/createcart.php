9c02dde857ce2a1a6acf168299ad742f0c08a863
Level420 <level420@protonmail.com>
2022-01-05 02:12:14 +0000

    hopefully my last commit for the night

--- api/v1/createcart.php
+++ api/v1/createcart.php
@@ -17,9 +17,14 @@ die();
 }
 
 else {
+returnobj = new \stdClass();
+returnobj->success = "true";
+returnobj->total = "0.00";
+$total = "0.00";
 $email = pg_fetch_result($executeem, 0, 0);
-$quertystring2 = pg_prepare($conn, 'query2', "INSERT INTO users (email) VALUES ($1)");
-$executeem2 = pg_execute($conn, 'query2', array($email));
-
+$quertystring2 = pg_prepare($conn, 'query2', "INSERT INTO users (email, total) VALUES ($1, $2)");
+$executeem2 = pg_execute($conn, 'query2', array($email, $total));
+returnobj = json_encode($returnobj);
+print($returnobj);
 }
 ?>
