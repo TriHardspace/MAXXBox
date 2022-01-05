1e3361c4501d38d794ade63d89ccc6d132cac833
Level420 <level420@protonmail.com>
2022-01-05 20:20:50 +0000

    wrong table

--- api/v1/createcart.php
+++ api/v1/createcart.php
@@ -22,7 +22,7 @@ $returnobj->success = "true";
 $returnobj->total = "0.00";
 $total = "0.00";
 $email = pg_fetch_result($executeem, 0, 0);
-$quertystring2 = pg_prepare($conn, 'query2', "INSERT INTO users (email, total) VALUES ($1, $2)");
+$quertystring2 = pg_prepare($conn, 'query2', "INSERT INTO cart (email, total) VALUES ($1, $2)");
 $executeem2 = pg_execute($conn, 'query2', array($email, $total));
 $returnobj = json_encode($returnobj);
 print($returnobj);
