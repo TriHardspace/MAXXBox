ffeed30e7baf4159c4ad538667a2a1338d799371
Level420 <level420@protonmail.com>
2022-01-06 02:38:33 +0000

    wrong column

--- api/v1/cartinfo.php
+++ api/v1/cartinfo.php
@@ -20,10 +20,10 @@ $preparestring2 = pg_prepare($conn, 'query2', 'SELECT (total, subtotal, hetzner_
 $query2 = pg_execute($conn, 'query2', array($email));
 $returnobj = new \stdClass();
 $returnobj->total = pg_fetch_result($query2, 0, 0);
-$returnobj->subtotal = pg_fetch_result($query2, 0, 1);
-$returnobj->hetzner_basic = pg_fetch_result($query2, 0, 2);
-$returnobj->hetzner_plus = pg_fetch_result($query2, 0, 3);
-$returnobj->hetzner_advanced = pg_fetch_result($query2, 0, 4);
+$returnobj->subtotal = pg_fetch_result($query2, 1, 0);
+$returnobj->hetzner_basic = pg_fetch_result($query2, 2, 0);
+$returnobj->hetzner_plus = pg_fetch_result($query2, 3, 0);
+$returnobj->hetzner_advanced = pg_fetch_result($query2, 4, 0);
 $returnobj = json_encode($returnobj);
 print($returnobj);
 die();
